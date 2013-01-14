915f16669ca07246ff6d5e622358f3ad2cdad664
Cesar Frias <cesar.frias@sieve.com.br>
2013-01-14 19:15:51 -0200

    Remove projeto nao utilizado

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='Requery',
     description="Simple way to store and use querys in database for use of DBA's",
     author='Ezequiel Bertti',
     author_email='ebertti@gmail.com',
-    packages=['project_requery', 'requery'],
+    packages=['requery'],
     install_requires=['Pygments==1.5'],
     )

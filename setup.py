af84819d80dddc748d84c9d103ff98ed54329dd8
Ezequiel Bertti <ezequiel.bertti@sieve.com.br>
2013-01-24 13:40:13 -0200

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ packages, package_data = setuplib.find_packages('requery')
 
 setup(
     name='requery',
-    version='0.2.1',
+    version='0.2.2',
     url='http://github.com/ebertti/requery/',
     author='ebertti',
     author_email='ebertti@gmail.com',

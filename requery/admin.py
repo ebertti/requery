a285c35501ea49c982ee9bf5903d4ef636be05a4
robertomoulin <roberto.moulin@sieve.com.br>
2013-08-14 12:00:59 -0300

    Adicionando campo de busca por nome no admin

--- requery/admin.py
+++ requery/admin.py
@@ -10,6 +10,8 @@ class QueryAdmin(admin.ModelAdmin):
     list_display = ('id', 'database', 'name', 'short_text', 'count_params', 'run')
     list_filter = ('database',)
 
+    search_fields = ('name',)
+
     def short_text(self, query):
         return query.text[:50]
 

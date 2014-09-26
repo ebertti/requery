a32a22db5aa9e2ddf72a3b50882b825f4e58b67a
Ezequiel Bertti <ebertti@gmail.com>
2014-09-26 17:45:23 -0300

    clean setup

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 # -*- coding:utf-8 -*-
 from setuptools import setup
 import setuplib
-
 from pip.req import parse_requirements
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements('requirements.txt')
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 reqs = [str(ir.req) for ir in install_reqs]
 
 packages, package_data = setuplib.find_packages('requery')

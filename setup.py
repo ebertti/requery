288c25bc7d0e9e56fdd275facea684f988784bce
Ezequiel Bertti <ebertti@gmail.com>
2014-09-27 02:21:33 -0300

    trying to fix setup with pip

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 # -*- coding:utf-8 -*-
 from setuptools import setup
-from pip.req import parse_requirements
-
-install_reqs = parse_requirements('requirements.txt')
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='requery',
-    version='0.3.2',
+    version='0.3.4',
     url='http://github.com/ebertti/requery/',
     author='Ezequiel Bertti',
     author_email='ebertti@gmail.com',
-    install_requires=reqs,
+    install_requires=['django>=1.7', 'Pygments>=1.6'],
     packages=['requery'],
     license='MIT License',
     platforms=['OS Independent'],
     description="Simple way to store and use querys in database for use of DBA's for Django Admin",
     long_description=(open('README.rst').read()),
-    keywords = ['query', 'dba', 'repository', 'report']
+    keywords = ['query', 'dba', 'repository', 'report', 'django']
 )
